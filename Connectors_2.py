b3c05351c96d29eac0520c26da2a875e55853b6a
Ravindra <raviraj3997@gamil.com>
2022-07-05 22:13:49 +0530

    dummy

--- Connectors_2.py
+++ Connectors_2.py
@@ -14,37 +14,49 @@ importlib.reload(Write_Connectors)
 # 	# Need some documentation? Run this with F5
 deck = constants.ABAQUS
 
-conn_sections = base.CollectEntities(deck, None, 'CONNECTOR_SECTION')
-conn_sec = conn_sections[0]
-connector_section_prop = conn_sec.get_entity_values(deck,('MID',))
+# conn_sections = base.CollectEntities(deck, None, 'CONNECTOR_SECTION')
+# conn_sec = conn_sections[0]
+# connector_section_prop = conn_sec.get_entity_values(deck,('MID',))
 
-print(connector_section_prop)
+# print(connector_section_prop)
 
-# 	sec_elems = base.CollectEntities(deck, conn_sec, None)
-	
-# 	behave = connector_section_prop['MID']
-# 	mat_name = behave._name
-	
-# 	dirr = dir(behave)
-# 	mat_elast = base.NameToEnts("^CONNECTOR ELASTICITY:"+mat_name)
-# 	conn_behave2 = base.CollectEntities(deck, None, 'CONNECTOR_ELASTICITY')
-# 	name = conn_behave2[0]._name
-# 	conn_behave2_prop = conn_behave2[0].get_entity_values(deck,('COMP','COMP(1)','NONLINEAR(1)','El.Stiff.(1)', 'Freq.(1)'))
-# 	stiff = conn_behave2_prop['El.Stiff.(1)']
-	
-# 	coordinate_sys = connector_section_prop['ORIENT_1']
-# 	c_name = coordinate_sys._name
-	
-# 	aa = 2
+# sec_elems = base.CollectEntities(deck, conn_sec, None)
+
+# behave = connector_section_prop['MID']
+# mat_name = behave._name
+
+# dirr = dir(behave)
+# mat_elast1 = base.NameToEnts("^CONNECTOR ELASTICITY:CURVE_BUSHING/10_BEHAVIOR$")
+# mat_elast2 = base.NameToEnts("^CONNECTOR ELASTICITY:CURVE_BUSHING/10_BEHAVIOR:1$")
+# print(mat_elast1)
+# print(mat_elast2)
+# elast1 = mat_elast1[0]
+# elast2 = mat_elast2[0]
+# print(elast1)
+# print(elast2)
+# conn_behave2 = base.elast2(deck, None, 'CONNECTOR_ELASTICITY')
+# # name = conn_behave2[0]._name
+# props1 = elast1.get_entity_values(deck,('COMP','COMP(1)','NONLINEAR(1)','El.Stiff.(1)', 'Freq.(1)'))
+# print(props1)
+
+# props2 = elast2.get_entity_values(deck,('COMP','COMP(1)','NONLINEAR(1)','El.Stiff.(1)', 'Freq.(1)'))
+# print(props2)
+
+# coordinate_sys = connector_section_prop['ORIENT_1']
+# c_name = coordinate_sys._name
+
+# aa = 2
 # if __name__ == '__main__':
-# 	main()
+#     main()
 
 import os
 
 connector_file = os.path.join(os.getcwd(), 'connectors_input.dat')
-
+conn_sections = base.CollectEntities(deck, None, 'CONNECTOR_SECTION')
 
 conns = Write_Connectors.ConnectorSections(conn_sections, connector_file)
 conns.write()
 
-x=22
+# x=22
+
+sda = 5
