c1b8f4a3dd9b10112c8d1c110ae6d6d9d41a0a9a
Ravindra <raviraj3997@gamil.com>
2022-07-05 22:13:16 +0530

    Updated connector matrial and section IDs

--- Write_Connectors.py
+++ Write_Connectors.py
@@ -1,4 +1,3 @@
-from cgitb import text
 import os
 import ansa
 from ansa import base
@@ -12,7 +11,8 @@ class ConnectorSections:
 		self.deck = constants.ABAQUS
 		self.output_file = out_file
 		self.conn_sections = conn_sections
-		self.connector_materials = []
+
+		self.connector_materials = ''
 
 	def write(self):
 		"""
@@ -20,26 +20,93 @@ class ConnectorSections:
 
 		"""
 		file = open(self.output_file, 'w')
+		file.write('*GET,REAL_MAX,RCON,0,0,NUM,MAX\n')
+		file.write('R_ID = REAL_MAX + 1\n')
+		file.write('R,R_ID\n')
+		file.write('\n')
+		file.write(f'REAL, R_ID\n')
+		file.write('\n')
 		file.close()
 
 		for conn_section in self.conn_sections:
 			conn_section_entities = conn_section.get_entity_values(self.deck,('MID','COMPONENT_1','COMPONENT_2','ORIENT_1','ORIENT_2' ))
+			# print(conn_section.card_fields(self.deck))
 			conn_mat = conn_section_entities['MID']
 			conn_comp1 = conn_section_entities['COMPONENT_1']
 			conn_comp2 = conn_section_entities['COMPONENT_2']
 
 			if conn_mat != None:
-				if conn_mat._name not in self.connector_materials:
-					self.connector_materials.append(conn_mat._name)
+				if conn_mat._name != self.connector_materials:
+					self.connector_materials = conn_mat._name
 					ConnectorMaterial(self.output_file, conn_mat, conn_comp1, conn_comp2).write()
 			else:
-				print("CONNECTOR SECTION with ELST:{} has no BEHAVIOR keyword. The connector element's behavior is determined by kinematic constraints only.")
+				# print(f"CONNECTOR SECTION with ELST:{conn_section._name} has no BEHAVIOR keyword. The connector element's behavior is determined by kinematic constraints only.")
+				file = open(self.output_file, 'a')
+
+				file.write('*GET,MAT_MAX,MAT,0,NUM,MAX\n')
+				file.write('M_ID = MAT_MAX+1\n')
+				file.write('MP,DENS,M_ID,0.0\n')
+				file.write('\n')
+				file.write(f'MAT, M_ID\n')
+				file.write('\n')
+
+				file.close()
 
 			ORIENT_1 = conn_section_entities['ORIENT_1']
 			ORIENT_2 = conn_section_entities['ORIENT_2']
+			ConnectorOrientation(self.output_file, ORIENT_1, ORIENT_2, conn_comp1,  conn_comp2).write()
 
+			ConnectorElement(self.output_file, conn_section, conn_comp1, conn_comp2).write()
+
+class ConnectorElement:
+	
+	def __init__(self, out_file, conn_section, conn_comp1, conn_comp2) -> None:
+		self.base = base
+		self.deck = constants.ABAQUS
+		self.output_file = out_file
+		self.conn_section = conn_section
+		self.conn_comp1 = conn_comp1
+		self.conn_comp2 = conn_comp2
+
+	def write(self):
+		file = open(self.output_file, 'a')
+		sec_elems = base.CollectEntities(self.deck, self.conn_section, None)
+		comps = ['CARTESIAN', 'CARDAN', 'BUSHING', 'AXIAL']
+		if sec_elems:
+			file.write('*get,TYPE_MAX,ETYP,0,NUM,MAX\n')
+			file.write('ETYP_ID = TYPE_MAX + 1\n')
+			file.write('ET,ETYP_ID,184\n')
+
+			if self.conn_comp1 in comps:
+				file.write('KEYOPT,E_id+1,1,16          ! General Joint\n')
 			
+			elif self.conn_comp1 == 'WELD':
+				file.write('KEYOPT,E_id+1,1,13          ! Weld Joint\n')
 
+			elif self.conn_comp1 == 'JOIN':
+				if self.conn_comp2 == None:
+					file.write('KEYOPT,E_id+1,1,0          ! Rigid Link Joint\n')
+				elif self.conn_comp2 == 'ALIGN':
+					file.write('KEYOPT,E_id+1,1,1          ! Rigid Beam\n')
+
+			elif self.conn_comp1 == 'BEAM':
+				file.write('KEYOPT,E_id+1,1,1          ! Rigid Beam\n')
+
+			file.write('\n')
+			file.write('TYPE, ETYP_ID\n')
+			file.write('\n')
+
+			file.write(f'! element of "*CONNECTOR SECTION, ELSET={self.conn_section._name}".\n')
+			for elem in sec_elems:
+				nodes = elem.get_entity_values(self.deck,('G1','G2'))
+				# print(nodes)
+				file.write(f'E,{nodes["G1"]._id},{nodes["G2"]._id}\n')
+				# print(f'E,{nodes["G1"]._id},{nodes["G2"]._id}\n')
+			
+			file.write('\n')
+				
+		file.close()
+		
 
 
 
@@ -60,28 +127,36 @@ class ConnectorMaterial:
 		"""
 
 		# Write Connector Elasticity:
-		conn_elast_list = base.NameToEnts("CONNECTOR ELASTICITY:"+self.material._name)
+		conn_elast_list = base.NameToEnts("^CONNECTOR ELASTICITY:"+self.material._name+"$")
 		if conn_elast_list:
 			conn_elast = conn_elast_list[0]
+			print(f"Writing connector material NAME: {self.material._name}")
 			self.write_connector_elasticity(conn_elast)
-			print(f"Writing connector material named TEst {self.material._name}")
-			print(f"Writing connector material named TEst {conn_elast._name}")
+			
+			# print(f"Writing connector material named TEst {conn_elast._name}")
 
 	def write_connector_elasticity(self, conn_elast):
+		# print(conn_elast)
 		file = open(self.output_file, 'a')
-		comps = ['CARTESIAN','CARDAN']
-		if self.conn_comp1 in comps and self.conn_comp2 in comps:
+		comps = ['CARTESIAN', 'CARDAN', 'BUSHING', 'AXIAL']
+		if self.conn_comp1 in comps or self.conn_comp2 in comps:
 			file.write('*GET,MAT_MAX,MAT,0,NUM,MAX\n')
 			file.write('M_ID = MAT_MAX+1\n')
 
 			props = ('NONLINEAR(1)','NONLINEAR(2)','NONLINEAR(3)','NONLINEAR(4)','NONLINEAR(5)','NONLINEAR(6)',)
+			# print(conn_elast._name)
 			check_linear = conn_elast.get_entity_values(self.deck, props)
 			all_linear = True
 			if any([True if i == 'YES' else False for i in list(check_linear.values())]):
 				all_linear = False
 			# print(f'all_linear = {all_linear}')
 
-			if conn_elast.get_entity_values(self.deck,('COMP',))['COMP'] == 'YES':
+			if not conn_elast.get_entity_values(self.deck,('COMP',)):
+				print(f"*CONNECTOR BEHAVIOR, NAME:{self.material._name} is not processed. \n")
+				file.write('MP,DENS,M_ID,0.0\n')
+				file.write(f"! *CONNECTOR BEHAVIOR, NAME:{self.material._name} is not processed. \n")
+
+			elif conn_elast.get_entity_values(self.deck,('COMP',))['COMP'] == 'YES':
 				comps_ampping = {1:1,2:7,3:12,4:16,5:19,6:21}
 				if all_linear:
 					file.write('\n')
@@ -89,10 +164,10 @@ class ConnectorMaterial:
 				for i in range(1,7):
 					props = (f'COMP({i})',f'DEP({i})',f'RIGID({i})',f'NONLINEAR({i})',f'El.Stiff.({i})',f'Freq.({i})',f'Force({i})',f'Rel.Disp.({i})',)
 					prop_dict = conn_elast.get_entity_values(self.deck, props)
-					if prop_dict[f'DEP({i})'] != 'YES' and prop_dict[f'RIGID({i})'] != 'YES':
+					if f'DEP({i})' in prop_dict.keys() and  prop_dict[f'DEP({i})'] != 'YES' and f'RIGID({i})' in prop_dict.keys() and prop_dict[f'RIGID({i})'] != 'YES':
 						if prop_dict[f'COMP({i})'] == 'YES':
 							if prop_dict[f'NONLINEAR({i})'] == 'YES':
-								print("Nonlinear Curve")
+								print(f"Nonlinear stiffness without dependent table is not processed. *CONNECTOR ELASTICITY, NAME:{conn_elast._name}.")
 							else:
 								if all_linear:
 									stiff = prop_dict[f'El.Stiff.({i})']
@@ -101,23 +176,87 @@ class ConnectorMaterial:
 									stiff = prop_dict[f'El.Stiff.({i})']
 									file.write('\n')
 									file.write(f'TB,JOIN,M_ID,1,3,JNS{i}\n')
-									file.write(f'TBDATA,,{-stiff}, -1.0 \n')
-									file.write(f'TBDATA,,0.0,0.0\n')
-									file.write(f'TBDATA,,{stiff}, 1.0\n')
+									file.write(f'TBPT,, -1.0,-{stiff} \n')
+									file.write(f'TBPT,,0.0,0.0\n')
+									file.write(f'TBPT,, 1.0,{stiff}\n')
 						else:
 							pass
 					else:
-						if prop_dict[f'DEP({i})'] != 'YES':
-							print(f"Tabular values are given for *CONNECTOR ELASTICITY with NAME:{conn_elast._name}. And this entity is not converted.")
-						if prop_dict[f'RIGID({i})'] != 'YES':
-							print(f"'RIGID' values are given for *CONNECTOR ELASTICITY with NAME:{conn_elast._name}. And this entity is not converted.")
+						if f'DEP({i})' in prop_dict.keys() and prop_dict[f'DEP({i})'] != 'YES':
+							if prop_dict[f'NONLINEAR({i})'] == 'YES':
+								pass
+							else:
+								print(f"Tabular values are given for *CONNECTOR ELASTICITY with NAME:{conn_elast._name}. And this entity is not processed.")
+
+						if f'RIGID({i})' in prop_dict.keys() and prop_dict[f'RIGID({i})'] == 'YES':
+							print(f"'RIGID' values are given for *CONNECTOR ELASTICITY with NAME:{conn_elast._name}. And this entity is not processed.")
 			else:
-				print(f"Matrix values are given for *CONNECTOR ELASTICITY with NAME:{conn_elast._name}. And this entity is not converted.")
+				print(f"Matrix values are given for *CONNECTOR ELASTICITY with NAME:{conn_elast._name}. And this entity is not processed.")
 			file.write('\n')
-			file.write(f'MAT, M_ID           !  Material Changed for following elements! \n')
+			file.write(f'MAT, M_ID\n')
 			file.write('\n')
 		else:
-			print(f'{self.conn_comp1} is not Translated for {self.material._name}')
+			print(f'{self.conn_comp1} is not processed for {self.material._name}')
 
 		file.close()
+
+
+class ConnectorOrientation:
+	def __init__(self, out_file, CSYS1, CSYS2, conn_comp1,  conn_comp2) -> None:
+		self.base = base
+		self.deck = constants.ABAQUS
+		self.output_file = out_file
+		self.csys_1 = CSYS1
+		self.csys_2 = CSYS2
+		self.conn_comp1 = conn_comp1
+		self.conn_comp2 = conn_comp2
+
+	def write(self):
+		general_joint_comps = ['CARTESIAN', 'CARDAN', 'BUSHING', 'AXIAL']
+
+		joint_type = ''
+		if self.conn_comp1 in general_joint_comps:
+			joint_type = 'gene'
+
+		elif self.conn_comp1 in 'WELD':
+			joint_type = 'WELD'
+
+
+		file = open(self.output_file, 'a')
+		file.write('\n')
+		file.write('*get,SEC_MAX,SECP,,NUM,MAX\n')	
+		file.write('SEC_ID = SEC_MAX + 1\n')		
+		file.write(f'sectype,SEC_ID, joint, {joint_type},\n')
+
+		if self.csys_1 != None:
+			id_1 = self.get_translated_id(self.csys_1)
+			if self.csys_2 == None:
+				id_2 = id_1
+			else:
+				id_2 = self.get_translated_id(self.csys_2)
+		elif self.csys_2 != None:
+			id_1 = 0
+			id_2 = self.get_translated_id(self.csys_2)
+		else:		
+			id_1 = 0
+			id_2 = 0
+
+		file.write(f'SECJOIN,,{id_1},{id_2}\n')
+		file.write('SECJOIN,RDOF\n')
+		file.write('\n')
+		file.write(f'SECNUM, SEC_ID\n')
+		file.write('\n')
+		file.close()
+
+	def get_translated_id(self, coord):
+		coords = base.CollectEntities(self.deck, None, 'ORIENTATION_R')
+		c_id  = coord._id
+		if c_id > 10:
+			csys = c_id
+		else:
+			if len(coords) > 10:
+				csys = c_id + len(coords)
+			else:
+				csys  = c_id + 10
+		return csys
 
